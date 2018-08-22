5e6962487ac358045a2c204ad14aa20d884be17c
Torvaney <torvaney@protonmail.com>
2018-08-23 00:13:59 +0100

    Add nelder-mead

---
+++ src/sandwich/core.clj
@@ -0,0 +1,153 @@
+(ns sandwich.core
+  (:require [clojure.math.combinatorics]
+            [clojure.core.matrix :as m]
+            [clojure.core.matrix.stats :as ms]))
+
+
+;; Initialise simplex
+
+(defn- find-b
+  [c n]
+  (* (/ c (* n (Math/sqrt 2)))
+     (- (Math/sqrt (+ n 1)) 1)))
+
+(defn- find-a
+  [c b]
+  (+ b (/ c (Math/sqrt 2))))
+
+(defn- init-simplex
+  "Initialise an equilateral n+1 dimensional simplex."
+  [x]
+  (let [c 1
+        n (count x)
+        b (find-b c n)
+        a (find-a c b)]
+    (->> (cons a (repeat (- n 1) b))
+         clojure.math.combinatorics/permutations
+         (mapv #(m/add x %))
+         (cons (m/matrix x)))))
+
+
+;; Simplex operations (finding new points)
+; Arguments are named according to the following convention:
+; xa: average of all points aside from the worst
+; xb: best point
+; xl: second worst ("lousy") point
+; xw: worst point
+
+(defn- reflect
+  "Reflect simplex and return the reflected point."
+  [p xa xw]
+  (m/add xa (m/mul p (m/sub xa xw))))
+
+(defn- expand
+  "Expand simplex and return the expanded point."
+  [p xr xa]
+  (m/add xr (m/mul p (m/sub xr xa))))
+
+(defn- contract-inside
+  "Contract simplex inside and return the contracted point."
+  [p xa xw]
+  (m/sub xa (m/mul p (m/sub xa xw))))
+
+(defn- contract-outside
+  "Contract simplex outside and return the contracted point."
+  [p xa xw]
+  (m/add xa (m/mul p (m/sub xa xw))))
+
+
+;; Evaluating the points on the simplex
+
+(defn- eval-point
+  "Evaluate a single point"
+  [f xi]
+  (assoc {} :x xi :f (f xi)))
+
+(defn- eval-simplex
+  "Evaluate a function at each point of a simplex."
+  [f x]
+  (mapv #(eval-point f %) x))
+
+
+;; Updating the simplex
+
+(defn- shrink-point
+  "Shrink a single point on the simplex."
+  [f p xb xi]
+  (->> (m/add xb (m/mul p (m/sub xi xb)))
+       (eval-point f)))
+
+(defn- shrink
+  "Shrink simplex."
+  [f p [xb & xs]]
+  (->> (cons xb (map #(shrink-point f p (:x xb) (:x %)) xs))
+       (sort-by :f)))
+
+(defn- accept-point
+  [sorted xi]
+  (->> (cons xi (drop-last sorted))
+       (sort-by :f)))
+
+(defn- expand-and-update
+  "Expand simplex and update accordingly"
+  [f p xa xb xr sorted]
+  (let [xe (eval-point f (expand p (:x xr) (:x xa)))]
+    (if (< (:f xe) (:f xb))
+      (accept-point sorted xe)
+      (accept-point sorted xr))))
+
+(defn- contract-inside-and-update
+  "Contract simplex inside and update accordingly"
+  [f p xa xw sorted]
+  (let [xc (eval-point f (contract-inside p (:x xa) (:x xw)))]
+    (if (< (:f xc) (:f xw))
+      (accept-point sorted xc)
+      (shrink f p sorted))))
+
+(defn- contract-outside-and-update
+  "Contract simplex inside and update accordingly"
+  [f p xa xr xw sorted]
+  (let [xc (eval-point f (contract-outside p (:x xa) (:x xw)))]
+    (if (<= (:f xc) (:f xr))
+      (accept-point sorted xc)
+      (shrink f p sorted))))
+
+
+;; Iterate
+
+(defn print-log [x] (do (println x) x))
+
+(defn- iter-simplex
+  "Find the next iteration of the simplex."
+  [f sorted]
+  (let [xw     (last sorted)
+        xl     (second (reverse sorted))
+        xb     (first sorted)
+        xa     (->> sorted drop-last (map :x) ms/mean m/matrix (eval-point f))
+        xr     (eval-point f (reflect 1 (:x xa) (:x xw)))]
+  (cond
+    (<  (:f xr) (:f xb)) (expand-and-update f 1 xa xb xr sorted)
+    (<= (:f xr) (:f xl)) (accept-point sorted xr)
+    (>  (:f xr) (:f xw)) (contract-inside-and-update f 0.5 xa xw sorted)
+    true                 (contract-outside-and-update f 0.5 xa xr xw sorted))))
+
+
+;; Do the thing
+
+(defn- converged?
+  [tol sorted]
+  (let [xb (first sorted)
+        xw (last sorted)]
+    (< (- (:f xw) (:f xb)) tol)))
+
+(defn nelder-mead
+  "Optimise f"
+  [f x0]
+  (let [x      (init-simplex x0)
+        sorted (->> x (eval-simplex f) (sort-by :f))]
+    (loop [s sorted]
+      (if (converged? 0.00000001 s)
+        (first s)
+        (recur (iter-simplex f s))))))
+
+
---
+++ test/sandwich/core_test.clj
@@ -0,0 +1,28 @@
+(ns sandwich.core-test
+  (:require [clojure.test :refer :all]
+            [sandwich.core :refer :all]))
+
+(defn- sq [x] (Math/pow x 2))
+
+(defn- build-rosenbrock
+  [a b]
+  (fn [[x y]]
+    (+ (sq (- a x))
+       (* b (sq (- y (sq x)))))))
+
+(defn- close? [tolerance x y]
+  (< (Math/abs (- x y)) tolerance))
+
+(defn- test-optim
+  [f op x-true x0]
+  (let [result (op f x0)
+        x-est  (:x result)]
+    (->> (mapv #(close? 0.001 %1 %2) x-true x-est)
+         (every? true?))))
+
+(deftest nelder-mead-test
+  (testing "Nelder-Mead minimisation"
+    (is (test-optim (build-rosenbrock 1 100) nelder-mead [1 1] [0   0]))
+    (is (test-optim (build-rosenbrock 1 100) nelder-mead [1 1] [5  -1]))
+    (is (test-optim (build-rosenbrock 1 100) nelder-mead [1 1] [10  2]))
+    (is (test-optim (build-rosenbrock 1 100) nelder-mead [1 1] [-1 -1]))))
