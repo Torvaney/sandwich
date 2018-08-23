6ad2fc99c8f6eb5490043d50cb40eea6ab0a0ae9
Torvaney <torvaney@protonmail.com>
2018-08-23 10:02:31 +0100

    Add code coverage

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/Torvaney/sandwich"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/math.combinatorics "0.1.4"]
-                 [net.mikera/core.matrix "0.62.0"]])
+                 [net.mikera/core.matrix "0.62.0"]]
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]])
