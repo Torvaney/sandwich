c4a3f40225ebcb265bb54dd16c28dbba98998252
Torvaney <torvaney@protonmail.com>
2018-08-23 09:54:41 +0100

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject sandwich "0.0.1"
+  :description "Gradient-free optimisation in clojure"
+  :url "https://github.com/Torvaney/sandwich"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [net.mikera/core.matrix "0.62.0"]])
